40b75c0c06ae4a51fe617ce2d82d1c7ff9f9d54a
evoxf1 <sahil2812@live.com>
2023-07-27 15:35:31 +0530

    first

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,20 @@
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "420a606e-397c-4ac6-b045-f4b5121b4994",
+    props.user.username,
+    props.user.secret
+  );
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
+};
+export default ChatsPage;
